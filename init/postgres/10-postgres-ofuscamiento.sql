101baceed9695f3393ac2b2a752d9d16ceaeb263
Thompson <thompsonadrian210@gmail.com>
2025-06-03 15:33:11 -0400

    indices

--- init/postgres/10-postgres-ofuscamiento.sql
+++ init/postgres/10-postgres-ofuscamiento.sql
@@ -1,11 +1,2 @@
 ----- ofuscamiento 
 
--- UPDATE jugadores
--- SET nombre = 'Jugador_' || id;
-
--- CREATE EXTENSION IF NOT EXISTS pgcrypto;
-
--- UPDATE usuarios
--- SET 
---     correo = 'xxxxx@' || SPLIT_PART(correo, '@', 2), -- solo dominio
---     password = crypt(password, gen_salt('bf'));      -- hasheando
