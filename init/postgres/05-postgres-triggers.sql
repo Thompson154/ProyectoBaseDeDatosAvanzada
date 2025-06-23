2e14bc8a81c24b2263d8293ecc75ef9be24663ec
Ernesto Juarez <ernestojuarezgutierrez03@gmail.com>
2025-06-22 20:12:19 -0400

    Review of Postgres

--- init/postgres/05-postgres-triggers.sql
+++ init/postgres/05-postgres-triggers.sql
@@ -1,16 +1,4 @@
-/* T1: daño al jugador */
-CREATE TRIGGER trg_player_damage
-AFTER INSERT ON combat_logs
-WHEN (NEW.target_type='player' AND NEW.action='damage')
-FOR EACH ROW EXECUTE FUNCTION fn_damage_player();
-
-/* T2: daño al zombi */
-CREATE TRIGGER trg_zombie_damage
-AFTER INSERT ON combat_logs
-WHEN (NEW.target_type='zombie' AND NEW.action='damage')
-FOR EACH ROW EXECUTE FUNCTION fn_damage_zombie();
-
-/* T3: level-up cuando se actualiza xp */
+/* T1: level-up cuando se actualiza xp */
 CREATE FUNCTION fn_level_up_trigger() RETURNS TRIGGER AS $$
 BEGIN
   PERFORM fn_check_level_up(NEW.player_id);
@@ -21,7 +9,7 @@ CREATE TRIGGER trg_check_lvl
 AFTER UPDATE OF xp ON player_stats
 FOR EACH ROW EXECUTE FUNCTION fn_level_up_trigger();
 
-/* T4: capacidad de inventario */
+/* T2: capacidad de inventario */
 CREATE FUNCTION fn_inv_capacity() RETURNS TRIGGER AS $$
 DECLARE total INT;
 BEGIN
@@ -40,8 +28,8 @@ CREATE TRIGGER trg_inv_capacity
 BEFORE INSERT OR UPDATE ON inventory_items
 FOR EACH ROW EXECUTE FUNCTION fn_inv_capacity();
 
-/* T5: reduce durabilidad del arma usada */
-CREATE FUNCTION fn_decrease_durability() RETURNS TRIGGER AS $$
+/* T3: reduce durabilidad del arma usada */
+/*CREATE FUNCTION fn_decrease_durability() RETURNS TRIGGER AS $$
 BEGIN
   IF NEW.weapon_id IS NOT NULL THEN
      UPDATE inventory_items
@@ -55,17 +43,4 @@ END$$ LANGUAGE plpgsql;
 CREATE TRIGGER trg_weapon_use
 AFTER INSERT ON combat_logs
 WHEN (NEW.action = 'damage')
-EXECUTE FUNCTION fn_decrease_durability();
-
-/* T6: bloquear suicidio de transacción de zombi repetido */
-CREATE FUNCTION fn_unique_kill() RETURNS TRIGGER AS $$
-BEGIN
-  IF EXISTS (SELECT 1 FROM kill_logs WHERE zombie_type=NEW.zombie_type AND player_id=NEW.player_id AND session_id=NEW.session_id) THEN
-     RETURN NULL; -- ignora duplicates
-  END IF;
-  RETURN NEW;
-END$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER trg_unique_kill
-BEFORE INSERT ON kill_logs
-FOR EACH ROW EXECUTE FUNCTION fn_unique_kill();
+EXECUTE FUNCTION fn_decrease_durability();*/
